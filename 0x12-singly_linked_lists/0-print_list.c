530b6b4e16ba8ad6b71809681817b17633d32f8d
Zvikomboreronyambuya <znyambuya@gmail.com>
2023-08-24 14:38:55 -0700

    Singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,26 +2,23 @@
 #include <stdio.h>
 
 /**
- * print_list - prints a list_t linked list
- *
- * @h: first node of linked list to print
- *
- * Return: number of nodes printed
+ * print_list - print all elements of `list_t` list
+ * @h: head of linked list
+ * Return: number of nodes
  */
 size_t print_list(const list_t *h)
 {
-	size_t len = 0;
+	const list_t *current;
+	size_t c;
 
-	if (h == NULL)
-		return (0);
-	while (h != NULL)
+	c = 0;
+	current = h;
+	while (current != NULL)
 	{
-		if (h->str != NULL)
-			printf("[%d] %s\n", h->len, h->str);
-		else
-			printf("[0] (nil)\n");
-		len++;
-		h = h->next;
+		printf("[%d] %s\n", current->len, current->str);
+		current = current->next;
+		c++;
 	}
-	return (len);
+
+	return (c);
 }
--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,9 +2,12 @@
 #include <stdlib.h>
 
 /**
- * turtle - prints before main
+ * print_intro  - This function is a constructor attribute, so it is executed
+ *               before the main function. It prints out a couple of lines
+ *               from a poem.
  */
-void __attribute__((constructor)) before_main(void)
+
+void __attribute__((constructor)) print_intro(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
