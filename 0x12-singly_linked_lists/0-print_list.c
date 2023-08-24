fef896c57460aeb220071c2c0cf61265073322ca
Zvikomboreronyambuya <znyambuya@gmail.com>
2023-08-24 14:54:08 -0700

    Singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,23 +3,25 @@
 
 /**
  * print_list - prints a list_t linked list
- * @h: first node of the linked list to print
+ *
+ * @h: first node of linked list to print
+ *
  * Return: number of nodes printed
  */
 size_t print_list(const list_t *h)
 {
 	size_t len = 0;
 
+	if (h == NULL)
+		return (0);
 	while (h != NULL)
 	{
-		if (h->str)
-			printf("[%zu] %s\n", h->len, h->str);
+		if (h->str != NULL)
+			printf("[%d] %s\n", h->len, h->str);
 		else
 			printf("[0] (nil)\n");
-
 		len++;
 		h = h->next;
 	}
-
 	return (len);
 }
