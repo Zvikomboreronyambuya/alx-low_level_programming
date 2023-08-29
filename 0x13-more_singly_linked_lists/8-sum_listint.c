2da7e4cef813df63397538b8a3f35eed2f120428
Zvikomboreronyambuya <znyambuya@gmail.com>
2023-08-29 01:15:46 -0700

    Advanced singly linked lists

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sums a listint list
+ *
+ * @head: head of list
+ *
+ * Return: sum of list
+ */
+int sum_listint(listint_t *head)
+{
+	int sum;
+
+	if (head == NULL)
+		return (0);
+
+	for (sum = 0; head != NULL; head = head->next)
+		sum += head->n;
+
+	return (sum);
+}
