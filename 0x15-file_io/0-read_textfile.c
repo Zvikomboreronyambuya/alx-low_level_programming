a3db00a6aef1aeadc2338a62283a7226e963c5ea
Zvikomboreronyambuya <znyambuya@gmail.com>
2023-09-04 07:29:58 -0700

    functions and programs

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,32 +8,35 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t rcount, wcount;
 	char *buffer;
+	int rc, wc; /* read & write character counter */
+	int fp;  /* File descriptor */
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
 
-	fd = open(filename, O_RDWR);
-	if (fd == -1)
+	fp = open(filename, O_RDONLY);
+	if (fp < 0)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
+	buffer = malloc(letters * sizeof(char));
 	if (buffer == NULL)
+		return (0);
+
+	rc = read(fp, buffer, letters);
+	if (rc < 0)
 	{
 		free(buffer);
 		return (0);
 	}
-	rcount = read(fd, buffer, letters);
-	if (rcount == -1)
-		return (0);
+	buffer[rc] = '\0';
+	close(fp);
 
-	wcount = write(STDOUT_FILENO, buffer, rcount);
-	if (wcount == -1 || rcount != wcount)
-		return (0);
+	wc = write(STDOUT_FILENO, buffer, rc);
 	free(buffer);
 
-	close(fd);
-	return (wcount);
+	if (wc < 0)
+		return (0);
+
+return (wc);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
